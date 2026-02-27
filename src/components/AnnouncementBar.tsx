c93bf8e26a095425c7e016a86febd0f49f5f4dcf
katrin <katrin.hansen239@gmail.com>
2026-02-27 13:58:48 +0100

    Hide AnnouncementBar

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const AnnouncementBar = () => {
   return (
-    <div className="w-full bg-white text-black text-center py-1 font-medium text-sm tracking-wide border-b border-gray-200 z-50 uppercase">
+    <div className="hidden w-full bg-white text-black text-center py-1 font-medium text-sm tracking-wide border-b border-gray-200 z-50 uppercase">
       <Link to="/updates" className="hover:underline block w-full h-full">
         Kalender 2026 ist da!
       </Link>
